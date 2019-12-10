8797303f47809a13cab2aaa88ec9f44e54229bdb
Eric Berry <coderberry@gmail.com>
2019-12-09 17:35:46 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Front = window.Front;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onConversation = this.onConversation.bind(this);
+  }
+
+  componentDidMount() {
+    Front.on("conversation", this.onConversation);
+  }
+
+  onConversation(data) {
+    console.log("Front data", data);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
